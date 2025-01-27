4af1f0c6ea87032615e14ae22a9747ad58d8eda1
MOON <133333114+M00N69@users.noreply.github.com>
2025-01-27 20:23:12 +0100

    Update IFSactionplan.py

--- IFSactionplan.py
+++ IFSactionplan.py
@@ -117,7 +117,7 @@ def generate_ai_recommendation_groq(non_conformity, guide_row):
     try:
         chat_completion = client.chat.completions.create(
             messages=messages,
-            model="llama-3.3-70b-versatile"
+            model="deepseek-r1-distill-llama-70b"
         )
         return chat_completion.choices[0].message.content
     except Exception as e:
